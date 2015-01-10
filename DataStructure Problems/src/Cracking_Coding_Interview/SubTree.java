57ca7c745883c8ca7713e1c4c36fd2a03d2ee043
rotate2050 <rushikesh.joshi@sjsu.edu>
2015-01-10 15:18:04 -0800

    Question 1 of Tree Form the Book

--- DataStructure Problems/src/Cracking_Coding_Interview/SubTree.java
+++ DataStructure Problems/src/Cracking_Coding_Interview/SubTree.java
@@ -1,5 +1,7 @@
 package Cracking_Coding_Interview;
 
+
+
 public class SubTree {
 	SubTree left_child;
 	SubTree right_child;
@@ -11,9 +13,109 @@ public class SubTree {
 	}
 }
 
-class Tree {
-	SubTree root;
+class BinarySearchTree {
+	SubTree root = null;
+	//int depth = 0;
+	
+	/**
+	 * This method insert the given element integer.
+	 * @param integer which you want to insert into Tree
+	 */
+	
 	void insert(int d) {
+		SubTree node = root;
+		SubTree temp = new SubTree(d);
+		SubTree prev=null;
+		// if Tree is empty
+		if (node==null) {
+			root = temp;
+		}
+		// otherwise find proper node where new node can be added
+		else {
+			while(node!=null) {
+				if (node.value > d) {
+					prev = node;
+					node = node.left_child;
+				}
+				else {
+					prev = node;
+					node = node.right_child;
+				}
+			}
+			if (prev.value > d) {
+				prev.left_child = temp;
+			}
+			else {
+				prev.right_child = temp;
+			}
+			//node = temp;
+		}
+	}
+	
+	void inorderTraversal(SubTree root) {
+		if (root==null) {
+			return;
+		}
+		inorderTraversal(root.left_child);	
+		System.out.println(root.value);
+		inorderTraversal(root.right_child);
+	}
+	
+	void postorderTraversal(SubTree root) {
+		if (root==null) {
+			return;
+		}
+		postorderTraversal(root.left_child);
+		postorderTraversal(root.right_child);
+		System.out.println(root.value);
+	}
+	
+	void preorderTraversal(SubTree root) {
+		if (root==null) {
+			return;
+		}
+		System.out.println(root.value);
+		preorderTraversal(root.left_child);
+		preorderTraversal(root.right_child);
 		
 	}
+	
+	int findDepth(SubTree root) {
+		int depth;
+		// if root of the tree is null then return 0
+		if (root==null) {
+			return 0;
+		}
+		// if not then calculate the height of the tree
+		depth = 1 + Math.max(findDepth(root.left_child), findDepth(root.right_child)); 
+		return depth;   //1 + Math.max(finddepth(root.left_child), finddepth(root.right_child));
+	}
+	
+	int findDepthbyKey(SubTree root,int key) {
+		int depth = 0;
+		// if root is null then return 0
+		if (root==null) {
+			return 0;
+		}
+		
+		while (root!=null) {
+			if (root.value > key) {
+				depth += 1;
+				root = root.left_child;
+			}
+			else if (root.value < key) {
+				depth += 1;
+				root = root.right_child ;
+			}
+			else {
+				return depth;
+			}
+		}
+		
+		return 0;
+	}
+	
+	/*void treebfs(SubTree root) {
+		
+	}*/
 }
---
+++ DataStructure Problems/src/Cracking_Coding_Interview/Tree_1.java
@@ -0,0 +1,76 @@
+package Cracking_Coding_Interview;
+
+public class Tree_1 extends BinarySearchTree{
+	/**
+	 * This method checks whether the given tree is balanced or not
+	 * @param pass the root of the given Tree for which you want to check whether
+	 * 		  its balanced or not
+	 * @return true if its balanced otherwise false
+	 */
+	boolean isTreeBalanced(SubTree root) {
+		// if tree is empty return false
+		if (root==null) {
+			return false;
+		}
+		// otherwise checks the difference of the min height and max height should not be greater than 1
+		if ((maxheightofTree(root) - minheightofTree(root))<1) {
+			return true;
+		}
+		
+		return false;
+	}
+	/**
+	 * This method finds the height of the Tree
+	 * @param root Tree
+	 * @return integer with 
+	 */
+	int maxheightofTree(SubTree root) {
+		int height;
+		// if node has no children then return 0 as a height
+		if (root==null) {
+			return 0;
+		}
+		// otherwise calculate the max height of the subtree
+		else {
+			height = 1 + Math.max(maxheightofTree(root.left_child),maxheightofTree(root.right_child));
+			return height;
+		}
+		 
+	}
+	/**
+	 * This method finds the minimum height of the Tree
+	 * @param root Object of SubTree class
+	 * @return minimum height of any leaf node in the Tree
+	 */
+	int minheightofTree(SubTree root) {
+		int height;
+		// if node has no children then return 0 as a height
+		if (root==null) {
+			return 0;
+		}
+		// otherwise calculate the man height of the subtree
+		else {
+			height = 1 + Math.min(minheightofTree(root.left_child), minheightofTree(root.right_child)); // if tree has children then calls method recursively for its children to find the height
+			return height;
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tree_1 bst = new Tree_1();
+		bst.insert(56);
+		bst.insert(36);
+		bst.insert(20);
+		bst.insert(40);
+		bst.insert(39);
+		bst.insert(50);
+		bst.insert(70);
+		bst.insert(75);
+		bst.insert(73);
+		int minheight = bst.minheightofTree(bst.root);
+		int maxheight = bst.maxheightofTree(bst.root);
+		System.out.println("MIN Height: " + minheight);
+		System.out.println("MAX Height: " + maxheight);
+	}
+
+}
