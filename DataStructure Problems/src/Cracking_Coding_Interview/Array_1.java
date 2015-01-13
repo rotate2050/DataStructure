e7fff5642b2274e4e125925f368e0ed684357ff6
rotate2050 <rushikesh.joshi@sjsu.edu>
2015-01-12 18:59:34 -0800

    Unique String without buffer string

--- DataStructure Problems/src/Cracking_Coding_Interview/Array_1.java
+++ DataStructure Problems/src/Cracking_Coding_Interview/Array_1.java
@@ -9,13 +9,56 @@ package Cracking_Coding_Interview;
  */
 public class Array_1 {
 
+	static boolean checkUniqueString(String s){
+		int bit_check = 0;
+		char[] char_string = s.toCharArray();
+		for (int i=0;i<s.length();i++) {
+			int aski = char_string[i] - 'A'; 
+			//int temp = 1 << aski;
+			if ((bit_check & (1 << aski)) > 0) 
+				return false;
+			else 
+				bit_check |= (1 << aski);
+		}
+		return true;
+	}
+	
+	static boolean isUniqueString(String s) {
+		int[] alphabet = new int[256];
+		char[] main_string = s.toCharArray();
+		
+		
+		boolean unique = true;
+		for (char c : main_string) {
+			int aski = c;
+			if (alphabet[aski]==0) {
+				alphabet[aski] = 1;
+			}
+			else {
+				//System.out.println("Duplicate Character");
+				if (alphabet[32]==1) {
+					
+				}
+				else {
+					unique = false;
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String s = "aBcDeFg";
+		/*
 		int[] alphabet = new int[256];
 		char[] main_string = args[0].toCharArray();
+		
+		
 		boolean unique = true;
 		for (char c : main_string) {
 			int aski = c;
@@ -36,7 +79,13 @@ public class Array_1 {
 		if(unique)
 			System.out.println("String with Unique Character");
 		else 
-			System.out.println("String with duplicate Character");
+			System.out.println("String with duplicate Character");*/
+		if (checkUniqueString(s)) {
+			System.out.println("Unique String");
+		}
+		else {
+			System.out.println("Not Unique");
+		}
 	}
 
 }
